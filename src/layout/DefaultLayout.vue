b77eba296cf8a2c289e0660020eb3e17f887962a
ildarH <ildar.habibrahmanov@icloud.com>
2024-09-23 22:22:46 +0500

    feat: add layout

---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import BaseSidebar from '@/components/base/Sidebar.vue'
+import BaseHeader from '@/components/base/Header/index.vue'
+import { CONFIG } from '@/config'
+import { useBreadcrumbs } from '@/composables/useBreadcrumbs'
+const ROUTE = CONFIG.ROUTE
+
+const { setBreadcrumbs } = useBreadcrumbs()
+
+setBreadcrumbs([
+  { text: 'Главная', href: ROUTE.HOME },
+  { text: 'Ученики', href: ROUTE.PUPILS },
+  { text: 'Документы', href: ROUTE.GROUPS, isActive: true }
+])
+</script>
+
+<template>
+  <div class="layout">
+    <BaseHeader class="header" />
+    <BaseSidebar class="sidebar" />
+    <RouterView class="content" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.layout {
+  display: grid;
+  grid-template-columns: 138px 1fr;
+  grid-template-rows: 72px 1fr;
+  grid-template-areas:
+    'sidebar header'
+    'sidebar content';
+  height: 100dvh;
+}
+.header {
+  grid-area: header;
+}
+.sidebar {
+  grid-area: sidebar;
+}
+.content {
+  grid-area: content;
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import UserCard from '@/components/base/UserCard/index.vue'
+import Documents from '@/components/base/Documents/index.vue'
+import AddDocumentModal from '@/components/Modals/AddDocument.vue'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/ui/dialog'
+import { useUser } from '@/composables/useUser'
+import { useDocuments } from '@/composables/useDocuments'
+import { useModal } from '@/composables/useModal'
+
+const { fetchUser, loading: isUserLoading, user } = useUser()
+const { fetchDocuments, loading: isDocumentsLoading, filteredDocuments } = useDocuments()
+
+fetchUser()
+fetchDocuments()
+</script>
+
+<template>
+  <div class="container mx-auto pt-6">
+    <div class="flex flex-col gap-8">
+      <UserCard :user="user" :loading="isUserLoading" />
+    </div>
+    <div class="mt-[30px] flex gap-[30px] justify-between overflow-x-scroll">
+      <Documents :documents="filteredDocuments" :loading="isDocumentsLoading" />
+    </div>
+  </div>
+</template>
