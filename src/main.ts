cd9c5c2d311539c2de50d2bc640675483bbe21f8
ildarH <ildar.habibrahmanov@icloud.com>
2024-09-23 23:35:20 +0500

    feat: remove shadcn select
    add VueSelect

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './assets/index.css'
 import VueDatePicker from '@vuepic/vue-datepicker'
+import vSelect from 'vue-select'
 import '@vuepic/vue-datepicker/dist/main.css'
+import 'vue-select/dist/vue-select.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -9,6 +11,7 @@ import router from './router'
 const app = createApp(App)
 
 app.component('VueDatePicker', VueDatePicker)
+app.component('VueSelect', vSelect)
 app.use(router)
 
 app.mount('#app')
